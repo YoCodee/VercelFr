68d32a0f125ec2fa3a0fee5e3d86dadcc76d4af8
unknown <yohannes.hn16@gmail.com>
2024-08-09 23:54:00 +0700

    i mau tidur

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -59,7 +59,7 @@ const HomePage = () => {
 
   return (
     <>
-    <div className='homePage flex  h-screen overflow-x-hidden  pl-[3%]'>
+    <div className='homePage flex  h-screen   pl-[3%]'>
     <div className="textContainer flex-[3] ">
 
         <div className="wrapper mdpr-24 flex flex-col  gap-12 h-full">
@@ -84,8 +84,8 @@ const HomePage = () => {
           <div className={open ? "menuIcon active z-50" : "menuIcon  sm:hidden flex    z-50"}>
           <img src="/menu.png" className='w-10 z-50 h-10 cursor-pointer' alt="Menu Icon" onClick={() => setOpen(prev => !prev)} />
         </div>
-        <div className={open ? "menu active z-10  bg-black flex flex-col text-white absolute w-[50%] text-xl justify-center items-center gap-24 right-0 bottom-0 top-0 h-screen overflow-hidden" : "menus bg-black flex-col absolute top-0 right-[-90%] h-full text-white w-[50%] flex transition-all ease-in-out justify-center items-center text-xl z-10"}>
-
+        <div className={open ? "menu active z-10  bg-black flex flex-col text-white absolute w-[50%] text-xl justify-center items-center gap-24 right-0 bottom-0 top-0 h-screen overflow-hidden" : "menus bg-black flex-col absolute top-0 right-[-90%] h-full text-white w-[50%] hidden  transition-all ease-in-out justify-center items-center text-xl z-10"}>
+       
           <a href="/" className='cursor-pointer'>Home</a>
           <a href="/" className='cursor-pointer'>About</a>
           <a href="/" className='cursor-pointer'>Contact</a>
